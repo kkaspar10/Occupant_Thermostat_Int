647952f1c6448c2c5305500eb81ad7eabc7f5ab6
Kingsley Nweye <etonwana@yahoo.com>
2023-08-15 17:53:02 -0500

    added pickle read function

--- citylearn/utilities.py
+++ citylearn/utilities.py
@@ -1,3 +1,5 @@
+import pickle
+from typing import Any
 import simplejson as json
 
 def read_json(filepath: str, **kwargs):
@@ -43,3 +45,22 @@ def write_json(filepath: str, dictionary: dict, **kwargs):
     kwargs = {'ignore_nan':True,'sort_keys':False,'default':str,'indent':2,**kwargs}
     with open(filepath,'w') as f:
         json.dump(dictionary,f,**kwargs)
+
+def read_pickle(filepath: str) -> Any:
+    """Return pickle object.
+    
+    Parameters
+    ----------
+    filepath : str
+       pathname of pickle file.
+       
+    Returns
+    -------
+    obj: Any
+        JSON document converted to dictionary.
+    """
+
+    with open(filepath, 'rb') as f:
+        data = pickle.load(f)
+
+    return data
