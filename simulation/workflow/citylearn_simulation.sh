0313575d29b66a4b598e6bfba57e6622866faab4
Kingsley Nweye <etonwana@yahoo.com>
2023-10-18 10:28:47 -0500

    refactored

--- simulation/workflow/citylearn_simulation.sh
+++ simulation/workflow/citylearn_simulation.sh
@@ -1,13 +1,6 @@
-python simulation/simulate.py general 1 simulate-citylearn citylearn RBC -x '2020' -w ComfortReward -e 1 -st 0 2159 -et 0 2159 -c
-python simulation/simulate.py general 1 simulate-citylearn citylearn RBC -x '2021' -w ComfortReward -e 1 -st 2160 4319 -et 2160 4319 -c 
-python simulation/simulate.py general 1 simulate-citylearn citylearn RBC -x '2022' -w ComfortReward -e 1 -st 4320 6479 -et 4320 6479 -c
-python simulation/simulate.py general 2 simulate-citylearn citylearn FullPowerHeatPumpRBC -x '2022' -w ComfortReward -e 1 -st 4320 6479 -et 4320 6479 -c
-python simulation/simulate.py general 2 simulate-citylearn citylearn ZeroPowerHeatPumpRBC -x '2022' -w ComfortReward -e 1 -st 4320 6479 -et 4320 6479 -c
-python simulation/simulate.py general 2 simulate-citylearn stable-baselines3 SAC -x '2022' -w ComfortReward -e 30 -st 0 2159 -st 2160 4319 -et 4320 6479 -c
-python simulation/simulate.py general 2 simulate-citylearn stable-baselines3 SAC -x '2022' -w AverageComfortReward -e 30 -st 0 2159 -st 2160 4319 -et 4320 6479 -c
-python simulation/simulate.py general 2 simulate-citylearn stable-baselines3 SAC -x '2022' -w MinimumComfortReward -e 30 -st 0 2159 -st 2160 4319 -et 4320 6479 -c
-python simulation/simulate.py general 2 simulate-citylearn citylearn SAC -x '2022' -w ComfortReward -e 30 -st 0 2159 -st 2160 4319 -et 4320 6479
-# python simulation/simulate.py general 2 simulate-citylearn citylearn MARLISA -x '2022' -w ComfortReward -e 10 -st 0 2159 -st 2160 4319 -et 4320 6479
-# python simulation/simulate.py general 2 simulate-citylearn citylearn MARLISA -x '2022' -w ComfortReward -e 10 -st 0 2159 -st 2160 4319 -et 4320 6479
-# python simulation/simulate.py general 3 simulate-citylearn stable-baselines3 SAC -x '2022' -w ComfortReward -e 10 -st 0 2159 -st 2160 4319 -et 4320 6479 -c
-# python simulation/simulate.py general 3 simulate-citylearn citylearn MARLISA -x '2022' -w ComfortReward -e 10 -st 0 2159 -st 2160 4319 -et 4320 6479
+python -m src.simulate general 1 simulate-citylearn citylearn RBC -x '2020' -e 1 -st 0 2159 -et 0 2159 -c
+python -m src.simulate general 1 simulate-citylearn citylearn RBC -x '2021' -e 1 -st 2160 4319 -et 2160 4319 -c 
+python -m src.simulate general 1 simulate-citylearn citylearn RBC -x '2022' -e 1 -st 4320 6479 -et 4320 6479 -c
+python -m src.simulate general 2 simulate-citylearn citylearn FullPowerHeatPumpRBC -x '2022' -e 1 -st 4320 6479 -et 4320 6479 -c
+python -m src.simulate general 2 simulate-citylearn citylearn ZeroPowerHeatPumpRBC -x '2022' -e 1 -st 4320 6479 -et 4320 6479 -c
+python -m src.simulate general 2 simulate-citylearn stable-baselines3 SAC -x 'building_all-2-2022' -w AverageComfortReward -e 15 -st 0 2159 -st 2160 4319 -et 4320 6479 -c
