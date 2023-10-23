d73ea9427d2b01a316791c1f8c14760390ae361d
Kingsley Nweye <etonwana@yahoo.com>
2023-10-23 11:34:27 -0500

    set to update setpoint frm current timestep to end

--- simulation/src/occ_citylearn.py
+++ simulation/src/occ_citylearn.py
@@ -140,7 +140,7 @@ class LogisticRegressionOccupantInteractionBuilding(OccupantInteractionBuilding)
         delta_input = [current_setpoint, previous_setpoint, previous_temperature - previous_setpoint]
         model_input = (interaction_input, delta_input)      
         setpoint_delta = self.occupant.predict(x=model_input)
-        self.energy_simulation.indoor_dry_bulb_temperature_set_point[self.time_step] = current_setpoint + setpoint_delta
+        self.energy_simulation.indoor_dry_bulb_temperature_set_point[self.time_step:] = current_setpoint + setpoint_delta
 
     def reset_data_sets(self):
         super().reset_data_sets()
